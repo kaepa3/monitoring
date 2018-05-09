c50d2dd401b11195905e859ae15693a7b8713fc4
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2018-05-09 18:47:41 +0900

    add video method

--- module/take_video.py
+++ module/take_video.py
@@ -1,6 +1,17 @@
+import picamera
+
 class TakeVideo(object):
-    def __init__(self, *args):
+    def __init__(self, time ,*args):
         super(TakeVideo, self).__init__(*args)
-    
-    def hoge(self):
-        return 11
+        self.camera = picamera.PiCamera()
+        self.time = time
+
+    def start_recording_with_time(self, file, time):
+        self.camera.start_recording(file)
+        self.camera.wait_recording(time)
+        self.camera.stop_recording()
+
+    def start_recording(self, time):
+        self.start_recording_with_time(file, self.time)
+
+
