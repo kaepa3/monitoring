f69b71cf254375af0eb509926fbaa82c4874150a
Takahiro Yamazaki <yamazaki@nvs-one.com>
2018-05-09 11:43:10 +0900

    Confirmation S3 connect

--- module/upload_server.py
+++ module/upload_server.py
@@ -1,10 +1,20 @@
+# -*- coding: utf-8 -*-
 import boto3
+import toml
 
 class UploadServer(object):
     def __init__(self, name, *args):
         super(UploadServer, self).__init__(*args)
-        self.bucket_name = name
-        self.s3 = boto3.resource('s3')
+        self.config = toml.load(open(name))
+        self.s3 = boto3.client('s3')
         
     def upload_file(self, file_name):
-        self.s3.Bucket(self.bucket_name).upload_file(file_name, file_name)
+        self.s3.upload_file(file_name, self.config["bucket_name"], file_name)
+
+    def list_file(self):
+        name = self.config["bucket_name"]
+        return self.s3.list_objects(Bucket=name).get("Contents")
+
+
+    def delete_file(self, file_name):
+        self.s3.delete_object(Bucket=self.config["bucket_name"], Key=file_name)
--- tests/test_upload_server.py
+++ tests/test_upload_server.py
@@ -1,9 +1,27 @@
+# -*- coding: utf-8 -*-
 import pytest
 
 from module.upload_server import UploadServer
 
+
 def test_classinitial():
-    name = "sv_name"
-    obj = UploadServer(name)
-    assert obj.bucket_name == name
+    obj = UploadServer("tests/test.toml")
+    assert obj.config["bucket_name"] == "test_bucket"
+    
+def test_upload():
+    upload_test_file = "conftest.py"
+    obj = UploadServer("s3.toml")
+    # 追加確認
+    obj.upload_file(upload_test_file)
+    list = obj.list_file()
+    flg = False
+    if list:
+        for v in list:
+            if upload_test_file == v.get("Key"):
+                flg = True
+
+    assert flg == True
+    # 削除確認
+    obj.delete_file(upload_test_file)
+
     
